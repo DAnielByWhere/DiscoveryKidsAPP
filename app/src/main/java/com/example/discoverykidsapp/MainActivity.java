9b03d1efdc19b563459ac4b20189df2fff228b6c
DAniel ByWhere <dan.bb2904@gmail.com>
2023-06-20 21:03:48 -0300

    V2.0.

--- app/src/main/java/com/example/discoverykidsapp/MainActivity.java
+++ app/src/main/java/com/example/discoverykidsapp/MainActivity.java
@@ -2,10 +2,11 @@ package com.example.discoverykidsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.RenderEffect;
+import android.content.Intent;
 import android.graphics.Shader;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,13 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-       ImageView imgMsMaker = findViewById(R.id.imgMsMaker);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            imgMsMaker.setRenderEffect(RenderEffect.createBlurEffect(10, 10, Shader.TileMode.MIRROR));
-        }
-
-
     }
+
+
 }
